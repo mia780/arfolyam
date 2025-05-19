7cf139433d964314d0b27259c67cb3a98b696e09
mia780 <varadymia@gmail.com>
2025-05-19 13:06:54 +0200

    fetch2

--- alap.js
+++ alap.js
@@ -1,1 +1,32 @@
-fetch
+fetch("https://api.exchangerate-api.com/v4/latest/usd")
+.then(x=>x.json)
+.then(y=>megjelenitUsb(y))
+
+function megjelenitUsb(y){
+    console.log(y)
+    document.getElementById("usd").innerHTML=y.rates.HUF+"ft"
+}
+
+fetch("https://api.exchangerate-api.com/v4/latest/eur")
+.then(x=>x.json)
+.then(y=>megjeleniteur(y))
+
+function megjeleniteur(y){
+    console.log(y)
+    document.getElementById("euro").innerHTML=y.rates.HUF+"ft"
+}
+fetch("https://api.exchangerate-api.com/v4/latest/chf")
+.then(x=>x.json)
+.then(y=>megjelenitchf(y))
+
+function megjelenitchf(y){
+    console.log(y)
+    document.getElementById("CHF").innerHTML=y.rates.HUF+"ft"
+}
+fetch("https://api.exchangerate-api.com/v4/latest/aud")
+.then(x=>x.json)
+.then(y=>megjelenitchf(y))
+function megjelenitaud(y){
+    console.log(y)
+    document.getElementById("aud").innerHTML=y.rates.HUF+"ft"
+}
